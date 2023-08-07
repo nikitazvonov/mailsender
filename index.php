c13980baab119bca58f990ba9d3e526c0e20f381
nikita <zvonovnikita42@gmail.com>
2023-08-07 11:50:02 +0700

    added functionallity and sent.html message

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <title>Mailsender</title>
+</head>
+<body>
+    <section>
+        <article>
+            <h1>Mailsender</h1>
+            <div>
+                <form action="index.php" method="POST">
+                    <label for="title">Адрес электронной почты</label>
+                    <input type="text" name="email" placeholder="Введите адрес электронной почты">
+
+                    <label for="title">Тема письма</label>
+                    <input type="text" name="title" placeholder="Введите тему письма">
+
+                    <label for="content">Содержание письма</label>
+                    <textarea name="content" cols="30" rows="10" placeholder="Содержание письма"></textarea>
+
+                    <input type="submit" name="submit">
+                </form>
+            </div>
+        </article>
+    </section>
+</body>
+</html>
+
 <?php
 
 $email = $_POST['email'];
@@ -9,20 +40,26 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
 $mail = new PHPMailer(true);
+
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->Port = 587;
 
-$mail->Username = 'zvonovnikita42@gmail.com';
-$mail->Password = 'ahkloploiunderfinishfinalinboxmessage42';
+$mail->Username = 'nikita@example.com';
+$mail->Password = 'password';
 
 $mail->setFrom($email);
-$mail->addAddress('warfacekem@gmail.com');
+$mail->addAddress('me@example.com');
 
+$mail->Subject = $title;
 $mail->Body = $content;
 
 $mail->send();
 
-echo 'sent';
+header('Location: sent.html');
+
+?>
